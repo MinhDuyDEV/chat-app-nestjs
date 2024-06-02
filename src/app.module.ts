fb649cefce28c6a54f35fe3518db374e560b1bf9
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2024-06-02 22:54:38 +0700

    Save user to database & hash passwords

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import entities from './utils/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: '.env.development' }),
+    ConfigModule.forRoot({ envFilePath: '.env.development', isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('MYSQL_DB_HOST'),
+        port: configService.get<number>('MYSQL_DB_PORT'),
+        username: configService.get<string>('MYSQL_DB_USERNAME'),
+        password: configService.get<string>('MYSQL_DB_PASSWORD'),
+        database: configService.get<string>('MYSQL_DB_NAME'),
+        synchronize: true,
+        entities,
+      }),
+      inject: [ConfigService],
+    }),
     AuthModule,
     UsersModule,
   ],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDetails } from 'src/auth/utils/types';
 import { IUserService } from './users';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/utils/typeorm';
+import { Repository } from 'typeorm';
+import { hashPassword } from 'src/utils/helpers';
 
 @Injectable()
 export class UserService implements IUserService {
-  createUser(userDetails: CreateUserDetails) {
-    console.log('🚀 ~ UserService ~ createUser ~ userDetails:', userDetails);
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async createUser(userDetails: CreateUserDetails) {
+    const existingUser = await this.userRepository.findOneBy({
+      email: userDetails.email,
+    });
+    if (existingUser)
+      throw new HttpException('User already exists', HttpStatus.CONFLICT);
+    const password = await hashPassword(userDetails.password);
+    const newUser = this.userRepository.create({ ...userDetails, password });
+    return this.userRepository.save(newUser);
   }
 }
