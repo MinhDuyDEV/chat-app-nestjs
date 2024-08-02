9f9f9b7106de55c7d43afc645d35ffc041e3289f
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2024-08-02 21:42:27 +0700

    Fetch messages by conversation id

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,12 +1,22 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 
 import { User } from 'src/utils/typeorm';
 import { IMessageService } from './messages';
 import { Services } from 'src/utils/constants';
 import { AuthUser } from 'src/utils/decorators';
+import { AuthenticatedGuard } from 'src/auth/utils/Guards';
 import { CreateMessageDto } from './dtos/CreateMessage.dto';
 
 @Controller('messages')
+@UseGuards(AuthenticatedGuard)
 export class MessagesController {
   constructor(
     @Inject(Services.MESSAGES) private readonly messageService: IMessageService,
@@ -18,5 +28,10 @@ export class MessagesController {
     @Body() createMessageDto: CreateMessageDto,
   ) {
     return this.messageService.createMessage({ ...createMessageDto, user });
+  }
+
+  @Get(':conversationId')
+  getMessagesFromConversation(@Param('conversationId') conversationId: number) {
+    return this.messageService.getMessagesByConversationId(conversationId);
   }
 }
